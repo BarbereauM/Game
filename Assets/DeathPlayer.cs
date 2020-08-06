8056c3ef2dacf5d26754671b50e14f72e5b7d7c4
BarbereauM <BARBEREAU.MAXIME@GMAIL.COM>
2020-08-06 19:41:52 +0200

    1.0 version

---
+++ Assets/DeathPlayer.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class DeathPlayer : MonoBehaviour
+{
+        private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Enemy"))
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
+
+
+
